7033fd2687efb0b5ba240b4b7889f56b59425cd5
puredanger <alex.miller@cognitect.com>
2017-09-19 18:46:21 -0500

    update specs for current state

--- src/main/clojure/clojure/tools/deps/alpha/specs.clj
+++ src/main/clojure/clojure/tools/deps/alpha/specs.clj
@@ -15,16 +15,6 @@
 (defmethod coord :mvn [_] (s/keys :opt-un [::version ::path ::exclusions]))
 (defmethod coord :file [_] (s/keys :opt-un [::path]))
 
-;; providers
-(s/def ::providers (s/keys :opt-un [::mvn ::file]))
-
-;; maven provider
-(s/def ::mvn (s/keys :opt [::repos]))
-(s/def ::repos (s/map-of ::repo-id ::repo))
-(s/def ::repo-id string?)
-(s/def ::repo (s/keys :opt-un [::url]))
-(s/def ::url string?)
-
 ;; resolve-deps args
 ;;   used to modify the expanded deps tree
 ;;   ::extra-deps - map of lib to coordinate added to the initial deps collection
@@ -35,19 +25,24 @@
 (s/def ::override-deps (s/map-of ::lib ::coord))
 (s/def ::default-deps (s/map-of ::lib ::coord))
 
-;; make-classpath overrides
-;;   specify a map of lib to path to use instead of the artifact found during resolution
+;; make-classpath args
+;;   used when constructing the classpath
+;;   ::classpath-overrides - map of lib to path to use instead of the artifact found during resolution
+;;   ::extra-paths - collection of extra paths to add to the classpath in addition to ::paths
+(s/def ::classpath-args (s/keys :opt-un [::classpath-overrides ::extra-deps]))
 (s/def ::classpath-overrides (s/map-of ::lib ::path))
+(s/def ::extra-paths (s/coll-of string? :kind vector? :into []))
 
 ;; deps map (format of the deps.edn file)
 ;;   ::deps - a map of library to coordinate (which has a provider type)
 ;;   ::providers - a map of artifact provider type to provider configuration
 ;;   ::resolve-args - a map with the same structure as the resolve-deps second arg
 ;;   ::aliases - a map from keyword (the alias) to a resolve-args map OR a make-classpath overrides map
-(s/def ::alias keyword?)
-(s/def ::aliases (s/map-of ::alias (s/or :resolve-deps ::resolve-args :make-classpath ::classpath-overrides)))
+(s/def ::paths (s/coll-of string? :kind vector? :into []))
 (s/def ::deps (s/map-of ::lib ::coord))
-(s/def ::deps-map (s/keys :opt-un [::deps ::providers ::aliases]))
+(s/def ::alias keyword?)
+(s/def ::aliases (s/map-of ::alias (s/or ::resolve-args ::classpath-args)))
+(s/def ::deps-map (s/keys :opt-un [::paths ::deps ::aliases]))
 
 ;; lib map
 ;;   a map of lib to resolved coordinate (a coord with a ::path) and dependent info
@@ -55,8 +50,14 @@
 (s/def ::resolved-coord (s/merge ::coord (s/keys :req-un [::path] :opt-un [::dependents])))
 (s/def ::lib-map (s/map-of ::lib ::resolved-coord))
 
-;; classpath
-(s/def ::classpath string?)
+;; Providers
+
+;; maven provider
+(s/def :mvn/repos (s/map-of ::repo-id ::repo))
+(s/def ::repo-id string?)
+(s/def ::repo (s/keys :opt-un [::url]))
+(s/def ::url string?)
+(s/def :mvn/local-repo string?)
 
 ;; API
 
@@ -65,13 +66,10 @@
   :ret ::lib-map)
 
 (s/fdef clojure.tools.deps.alpha/make-classpath
-  :args (s/cat :libs ::lib-map :overrides ::classpath-overrides)
-  :ret ::classpath)
+  :args (s/cat :libs ::lib-map :paths ::paths :classpath-args ::classpath-args)
+  :ret string?)
 
 (comment
-  (s/valid? ::providers
-    {:providers {:mvn {:repos {"central" {:url "https://repo1.maven.org/maven2/"}
-                               "clojars" {:url "https://clojars.org/repo/"}}}}})
   (require '[clojure.spec.test.alpha :as stest])
   (stest/instrument (stest/enumerate-namespace 'clojure.tools.deps.alpha))
   )
