f488ae1cbd74703931346d1ee9340f54bf5c22ec
Alex Miller <alex.miller@cognitect.com>
2019-09-17 20:08:29 -0500

    add some scratch code to recursively verify deps.edn files

--- src/main/clojure/clojure/tools/deps/alpha/specs.clj
+++ src/main/clojure/clojure/tools/deps/alpha/specs.clj
@@ -88,6 +88,27 @@
   :ret string?)
 
 (comment
-  (require '[clojure.spec.test.alpha :as stest])
+  ;; some scratch code to recursively check every deps.edn under
+  ;; a root directory whether it's valid against the specs
+  (require
+    '[clojure.spec.test.alpha :as stest]
+    '[clojure.tools.deps.alpha.reader :as trdr]
+    '[clojure.java.io :as jio])
+  (import '[java.nio.file Files Paths FileVisitor FileVisitResult])
   (stest/instrument (stest/enumerate-namespace 'clojure.tools.deps.alpha))
+
+  (Files/walkFileTree
+    (Paths/get "../" (into-array String []))
+    (reify FileVisitor
+      (postVisitDirectory [_ dir ex] FileVisitResult/CONTINUE)
+      (preVisitDirectory [_ dir attrs] FileVisitResult/CONTINUE)
+      (visitFileFailed [_ f ex] FileVisitResult/CONTINUE)
+      (visitFile [_ f attrs]
+        (when (.endsWith (str f) "/deps.edn")
+          (print "Checking" (str f))
+          (let [v (s/valid? ::deps-map (#'trdr/slurp-edn-map (.toFile f)))]
+            (println ":" v)
+            (when-not v
+              (s/explain ::deps-map (#'trdr/slurp-edn-map (.toFile f))))))
+        FileVisitResult/CONTINUE)))
   )
