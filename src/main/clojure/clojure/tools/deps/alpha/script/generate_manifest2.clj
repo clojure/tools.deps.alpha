3ca851abfb65c90a440f023b15d78ffb690221d4
Alex Miller <alex.miller@cognitect.com>
2020-01-17 15:22:45 -0600

    fix ns meta

--- src/main/clojure/clojure/tools/deps/alpha/script/generate_manifest2.clj
+++ src/main/clojure/clojure/tools/deps/alpha/script/generate_manifest2.clj
@@ -6,7 +6,7 @@
 ;   the terms of this license.
 ;   You must not remove this notice, or any other, from this software.
 
-(ns ^{skip-wiki true}
+(ns ^{:skip-wiki true}
    clojure.tools.deps.alpha.script.generate-manifest2
   (:require [clojure.java.io :as jio]
             [clojure.tools.cli :as cli]
