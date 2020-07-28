0c57e5eb9256120684dcedc1e270598ff58fa461
Alex Miller <alex.miller@cognitect.com>
2020-07-28 13:02:32 -0500

    remove code deps for exec and minimize Clojure version constraints

--- src/main/clojure/clojure/tools/deps/alpha/exec.clj
+++ src/main/clojure/clojure/tools/deps/alpha/exec.clj
@@ -8,17 +8,16 @@
 
 (ns clojure.tools.deps.alpha.exec
   (:require
+    ;; NOTE: ONLY depend on Clojure core, loaded in user's classpath so can't have any deps
     [clojure.edn :as edn]
     [clojure.java.io :as jio]
-    [clojure.string :as str]
-    [clojure.tools.deps.alpha :as deps]
-    [clojure.tools.deps.alpha.script.parse :as parse]))
+    [clojure.string :as str]))
 
 (defn- read-basis
   []
   (when-let [f (jio/file (System/getProperty "clojure.basis"))]
     (if (and f (.exists f))
-      (deps/slurp-deps f)
+      (-> f slurp edn/read-string)
       (throw (IllegalArgumentException. "No basis declared in clojure.basis system property")))))
 
 (defn- check-first
@@ -39,10 +38,20 @@
     (cond-> fread
       (seq args) (assoc :overrides (mapv edn/read-string args)))))
 
+(defn- requiring-resolve'
+  ;; copied and modified from core to remove constraints on Clojure 1.10.x
+  [sym]
+  (if (nil? (namespace sym))
+    (throw (IllegalArgumentException. (str "Not a qualified symbol: " sym)))
+    (or (resolve sym)
+      (do
+        (-> sym namespace symbol require)
+        (resolve sym)))))
+
 (defn exec
   "Resolve and execute the function f (a symbol) with args"
   [f & args]
-  (apply (requiring-resolve f) args))
+  (apply (requiring-resolve' f) args))
 
 (defn- apply-overrides
   [args overrides]
@@ -67,9 +76,3 @@
   [& args]
   (let [{:keys [alias overrides] :as parsed} (parse-args args)]
     (exec-alias alias overrides)))
-
-(comment
-  (-main "-X:foo" "[:y :z]" "1")
-  (-main "-X:foo" ":bar")
-  (apply-overrides {:x 1} [:x 2 [:y :z] 3])
-  )
