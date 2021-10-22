8e09c9a1427227c586fcdf9df404922e451c518f
Alex Miller <alex.miller@cognitect.com>
2021-10-22 15:21:58 -0500

    TDEPS-210 Update specs

--- src/main/clojure/clojure/tools/deps/alpha/specs.clj
+++ src/main/clojure/clojure/tools/deps/alpha/specs.clj
@@ -9,31 +9,53 @@
 (ns clojure.tools.deps.alpha.specs
   (:require [clojure.spec.alpha :as s]))
 
-;; library, like org.clojure/clojure
+;;;; generic types
+
 (s/def ::lib symbol?)
-
-;; coordinates
-
 (s/def ::path string?)
+(s/def ::alias keyword?)
 
+;;;; coordinates
+
+;; generic coord attributes
+(s/def :deps/root ::path)
+(s/def :deps/manifest keyword?)
+(s/def :deps/exclusions (s/coll-of ::lib))
+(s/def :deps/coord (s/keys :opt [:deps/root :deps/manifest] :opt-un [:deps/exclusions]))
+
+;; maven coords
 (s/def :mvn/version string?)
-(s/def ::exclusions (s/coll-of ::lib))
-(s/def :mvn/coord (s/keys :req [:mvn/version] :opt-un [::path ::exclusions]))
+(s/def :mvn/coord (s/merge :deps/coord
+                    (s/keys :req [:mvn/version])))
 
+;; local coords
 (s/def :local/root string?)
-(s/def :local/coord (s/keys :req [:local/root] :opt-un [::path]))
+(s/def :local/coord (s/merge :deps/coord
+                      (s/keys :req [:local/root])))
 
+;; git coords
 (s/def :git/url string?)
 (s/def :git/sha string?)
-(s/def :git/coord (s/keys :req [:git/url] :req-un [:git/sha]))
+(s/def :git/tag string?)
+(s/def :git/coord (s/merge :deps/coord
+                    (s/keys :opt [:git/url :git/sha :git/tag])))
 
-;; should this become a multipec?
-(s/def ::coord (s/or :mvn :mvn/coord
-                     :local :local/coord
-                     :git :git/coord))
+;; should this become a multispec?
+(s/def ::coord (s/nilable
+                 (s/or :mvn :mvn/coord
+                       :local :local/coord
+                       :git :git/coord)))
 
-;; resolve-deps args
-;;   used to modify the expanded deps tree
+(s/def ::path-ref (s/or :path ::path :alias ::alias))
+(s/def :aliased/paths (s/coll-of ::path-ref :kind vector? :into []))
+
+;; tool args
+;;   ::replace-deps - map of lib to coordinate to replace the
+(s/def ::tool-args (s/keys :opt-un [::replace-deps ::replace-paths ::deps ::paths]))
+(s/def ::replace-deps (s/map-of ::lib ::coord))
+(s/def ::replace-paths :aliased/paths)
+
+;; resolve-deps args - used to modify the expanded deps tree
 ;;   ::extra-deps - map of lib to coordinate added to the initial deps collection
 ;;   ::override-deps - map of lib to coordinate to use instead of the coord found during expansion
 ;;   ::default-deps - map of lib to coordinate to use if no coord is specified in extension
@@ -41,41 +63,59 @@
 (s/def ::extra-deps (s/map-of ::lib ::coord))
 (s/def ::override-deps (s/map-of ::lib ::coord))
 (s/def ::default-deps (s/map-of ::lib ::coord))
+(s/def ::threads pos-int?)
+(s/def ::trace boolean?)
 
-;; make-classpath args
-;;   used when constructing the classpath
+;; make-classpath args - used when constructing the classpath
 ;;   ::classpath-overrides - map of lib to path to use instead of the artifact found during resolution
 ;;   ::extra-paths - collection of extra paths to add to the classpath in addition to ::paths
 (s/def ::classpath-args (s/keys :opt-un [::classpath-overrides ::extra-paths]))
 (s/def ::classpath-overrides (s/map-of ::lib ::path))
-(s/def ::extra-paths (s/coll-of string? :kind vector? :into []))
+(s/def ::extra-paths :aliased/paths)
+
+;; exec args - used when executing a function with -X or -T
+;;   ::exec-args - map of default function args
+;;   ::exec-fn - default function symbol
+;;   ::ns-default - default namespace to use when resolving functions
+;;   ::ns-aliases - map of alias to namespace to use when resolving functions
+(s/def ::exec-args (s/keys :opt-un [::exec-args ::exec-fn ::ns-default ::ns-aliases]))
+(s/def ::exec-args (s/nilable map?))
+(s/def ::ns-default simple-symbol?)
+(s/def ::ns-aliases (s/map-of simple-symbol? simple-symbol?))
 
 ;; deps map (format of the deps.edn file)
-(s/def ::alias keyword?)
-(s/def ::paths (s/coll-of (s/or :path ::path :alias ::alias) :kind vector? :into []))
+(s/def ::paths :aliased/paths)
 (s/def ::deps (s/map-of ::lib ::coord))
 (s/def ::aliases (s/map-of ::alias any?))
-(s/def ::deps-map (s/keys :opt-un [::paths ::deps ::aliases]))
+(s/def ::deps-map (s/keys
+                    :opt-un [::paths ::deps ::aliases]
+                    :opt [:mvn/repos :mvn/local-repo :tools/usage :deps/prep-lib]))
 
 ;; lib map
 ;;   a map of lib to resolved coordinate (a coord with a ::path) and dependent info
 (s/def ::dependents (s/coll-of ::lib))
-(s/def ::resolved-coord (s/merge ::coord (s/keys :req-un [::paths] :opt-un [::dependents])))
+(s/def ::resolved-coord (s/merge ::coord (s/keys :opt-un [:aliased/paths ::dependents])))
 (s/def ::lib-map (s/map-of ::lib ::resolved-coord))
 
 ;; classpath
 (s/def ::classpath string?)
 
-(s/def ::run (s/keys :req-un [::deps-map ::lib-map ::classpath]))
-
 ;; Procurers
 
-;; maven 
+;; Maven
 (s/def :mvn/repos (s/map-of ::repo-id ::repo))
 (s/def ::repo-id string?)
-(s/def ::repo (s/keys :opt-un [::url]))
+(s/def ::repo (s/nilable (s/keys :opt-un [::url])))
 (s/def ::url string?)
 (s/def :mvn/local-repo string?)
+
+;; Tool usage
+(s/def :tools/usage (s/keys :opt-un [::ns-default ::ns-aliases]))
+
+;; Prep lib
+(s/def :deps/prep-lib (s/keys :require-un [:prep/ensure ::alias :prep/fn]))
+(s/def :prep/ensure ::path)
+(s/def :prep/fn symbol?)
 
 ;; API
 
@@ -83,8 +123,12 @@
   :args (s/cat :deps ::deps-map :options ::resolve-args)
   :ret ::lib-map)
 
+(s/fdef clojure.tools.deps.alpha/make-classpath-map
+  :args (s/cat :deps ::deps-map, :libs ::lib-map, :classpath-args ::claspath-args)
+  :ret map?)
+
 (s/fdef clojure.tools.deps.alpha/make-classpath
-  :args (s/cat :libs ::lib-map :paths ::paths :classpath-args ::classpath-args)
+  :args (s/cat :libs ::lib-map, :paths ::paths, :classpath-args ::classpath-args)
   :ret string?)
 
 (comment
